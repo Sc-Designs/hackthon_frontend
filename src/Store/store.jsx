0869770489907a663ba54ada2ac640828b640f1e
Suvam Chakraborti <nfgaming4523@gmail.com>
2025-06-15 18:24:52 +0530

    Update in Reducers

--- src/Store/store.jsx
+++ src/Store/store.jsx
@@ -1,22 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import {UserReducer} from "./Reducers/UserReducer"
 import { DoctorReducer } from "./Reducers/DoctorReducer";
-<<<<<<< HEAD
 import { AllDoctorReducer } from "./Reducers/AllDoctorsReducer";
-=======
 import Fundraise from"./Reducers/fundraiseReducer";
->>>>>>> rubultanti
 export const store = configureStore({
     reducer:{
         // all reducers
         // key: value of key
         User : UserReducer,
         Doctor: DoctorReducer,
-<<<<<<< HEAD
-        AllDoctors: AllDoctorReducer
-=======
+        AllDoctors: AllDoctorReducer,
         fundraise:Fundraise
-
->>>>>>> rubultanti
     }
 })
